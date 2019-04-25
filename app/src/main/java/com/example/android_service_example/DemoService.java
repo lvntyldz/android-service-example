b8781e9ff117dfed74df8e76aa468e326353235a
levent yıldız <leventyildiz@leventyildiz.local>
2019-04-26 00:46:50 +0300

    demo service implementation

---
+++ app/src/main/java/com/example/android_service_example/DemoService.java
@@ -0,0 +1,28 @@
+package com.example.android_service_example;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import org.jetbrains.annotations.Nullable;
+
+public class DemoService extends Service {
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        Toast.makeText(this, "Service started", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDestroy() {
+        Toast.makeText(this, "Service stopped", Toast.LENGTH_SHORT).show();
+    }
+}
--- app/src/main/java/com/example/android_service_example/MainActivity.java
+++ app/src/main/java/com/example/android_service_example/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.android_service_example;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonStart;
+    private Button buttonStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        buttonStart = (Button) findViewById(R.id.button);
+        buttonStop = (Button) findViewById(R.id.button2);
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this, DemoService.class));
+            }
+        });
+
+
+        buttonStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(new Intent(MainActivity.this, DemoService.class));
+            }
+        });
+
     }
 }
